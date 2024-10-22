89f60e1430a2cb2255525d03369c8d232813c4eb
roqaya ashraf <your_email@example.com>
2024-10-22 19:07:16 +0300

    dashboard navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,80 +1,81 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   // WITH THE LAYOUT
   {
-    path: "",
+    path: '',
     loadComponent: () =>
-      import("./layouts/blank-layout/blank-layout.component").then(
-        (m) => m.BlankLayoutComponent,
+      import('./layouts/blank-layout/blank-layout.component').then(
+        (m) => m.BlankLayoutComponent
       ),
     children: [
       {
-
-        path: "",
+        path: '',
         loadComponent: () =>
-          import("./components/home/home.component").then(
-            (m) => m.HomeComponent,
+          import('./components/home/home.component').then(
+            (m) => m.HomeComponent
           ),
       },
       {
-        path: "home",
+        path: 'home',
         loadComponent: () =>
-          import("./components/home/home.component").then(
-            (m) => m.HomeComponent,
+          import('./components/home/home.component').then(
+            (m) => m.HomeComponent
           ),
       },
       {
-        path: "blogs",
+        path: 'dashboard',
         loadComponent: () =>
-          import("./components/blogs/blogs.component").then(
-            (m) => m.BlogsComponent,
+          import('./components/dashboard/dashboard.component').then(
+            (m) => m.DashboardComponent
           ),
       },
       {
-        path: "contact",
+        path: 'blogs',
         loadComponent: () =>
-          import("./components/contact/contact.component").then(
-            (m) => m.ContactComponent,
+          import('./components/blogs/blogs.component').then(
+            (m) => m.BlogsComponent
+          ),
+      },
+      {
+        path: 'contact',
+        loadComponent: () =>
+          import('./components/contact/contact.component').then(
+            (m) => m.ContactComponent
           ),
       },
     ],
   },
   // WITHOUT THE LAYOUT
   {
-    path: "dashboard",
-    loadComponent: () =>
-      import("./components/dashboard/dashboard.component").then(
-        (m) => m.DashboardComponent,
-      ),
     children: [
       {
-        path: "",
+        path: '',
         loadComponent: () =>
           import(
-            "./components/dashboard-content/dashboard-content.component"
+            './components/dashboard-content/dashboard-content.component'
           ).then((m) => m.DashboardContentComponent),
       },
       {
-        path: "profile",
+        path: 'profile',
         loadComponent: () =>
-          import("./components/profile/profile.component").then(
-            (m) => m.ProfileComponent,
+          import('./components/profile/profile.component').then(
+            (m) => m.ProfileComponent
           ),
       },
     ],
   },
   {
-    path: "template",
+    path: 'template',
     loadComponent: () =>
-      import("./components/template/template.component").then(
-        (m) => m.TemplateComponent,
+      import('./components/template/template.component').then(
+        (m) => m.TemplateComponent
       ),
   },
   {
-    path: "**",
-    redirectTo: "home",
+    path: '**',
+    redirectTo: 'home',
   },
 ];
 
@@ -82,4 +83,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
